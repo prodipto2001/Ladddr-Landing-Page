e699b4fb62d9e284dfa691547991d267f9fcb649
prodipto2001 <prodiptoofficial2001@gmail.com>
2025-03-11 23:53:19 +0530

    Updated feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,11 +37,28 @@ export default function Home() {
           <h1 className="text-center text-[#7159f3] text-8xl font-bold font-['Space_Grotesk'] leading-[93.59px] mb-8">
             BASED DESIGN FOR A<br/>TOKENIZED WORLD
           </h1>
+
           <p className="text-center justify-start text-[#373737] text-4xl font-bold font-['Space_Grotesk']">NO CAP. NO MID. JUST VIBES.</p>
 
           <MintButton className="pt-14" />
-          
-        </div>
+
+          <div className="w-[60vw] max-w-full flex flex-col sm:flex-row justify-between p-4">
+            <div className="p-6 bg-[#ded8ff]/60 rounded-3xl backdrop-blur-[2px] inline-flex justify-start items-center gap-3">
+              <div className="text-center justify-start text-[#5b43dd] text-4xl font-bold font-['Space_Grotesk'] capitalize">500+</div>
+              <div className="justify-start text-[#5b43dd] text-2xl font-normal font-['Space_Grotesk'] uppercase tracking-wide">Screens<br/>Designed</div>
+            </div>
+
+            <div className="p-6 bg-[#ded8ff]/60 rounded-3xl backdrop-blur-[2px] inline-flex justify-start items-center gap-3">
+              <div className="text-center justify-start text-[#5b43dd] text-4xl font-bold font-['Space_Grotesk'] capitalize">500+</div>
+              <div className="justify-start text-[#5b43dd] text-2xl font-normal font-['Space_Grotesk'] uppercase tracking-wide">Screens<br/>Designed</div>
+            </div>
+
+            <div className="p-6 bg-[#ded8ff]/60 rounded-3xl backdrop-blur-[2px] inline-flex justify-start items-center gap-3">
+              <div className="text-center justify-start text-[#5b43dd] text-4xl font-bold font-['Space_Grotesk'] capitalize">500+</div>
+              <div className="justify-start text-[#5b43dd] text-2xl font-normal font-['Space_Grotesk'] uppercase tracking-wide">Screens<br/>Designed</div>
+            </div>
+          </div> 
+        </div>      
       </section>
     </div>
   );
