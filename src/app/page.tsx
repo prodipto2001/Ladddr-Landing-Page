091ea13e48a63fe2431a10de49a1035260f3d5e8
prodipto2001 <prodiptoofficial2001@gmail.com>
2025-03-14 01:08:59 +0530

    Updated feature

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,33 +4,28 @@ import Hero from "./components/Hero";
 
 export default function Home() {
   return (
-    <div className="relative min-h-screen bg-white px-4 sm:px-6 md:px-12 lg:px-24 xl:px-[180px]">
-      {/* Fixed Background Image */}
-      <div className="fixed inset-0 z-10">
-        <Image
-          src="/background-image.svg"
-          alt="Background"
-          fill
-          className="object-cover opacity-20"
-          priority
-          quality={100}
-        />
-      </div>
-
-      {/* Bottom Background Image - Adjusted for mobile */}
-      <div className="fixed -bottom-40 sm:-bottom-80 md:-bottom-120 lg:-bottom-180 xl:-bottom-240 left-0 right-0 z-0">
+    <div className="fixed inset-0 overflow-hidden bg-white">
+      {/* Background Image */}
+      
+      {/* Bottom Background Image */}
+      <div className="absolute -bottom-200 left-0 right-0 z-0 transform translate-y-1/4 sm:translate-y-1/5 md:translate-y-1/6 lg:translate-y-1/8">
         <Image
           src="/Mask-group.svg"
           alt="Bottom Background"
-          width={1080}
+          width={1440}
           height={1080}
           className="w-full object-contain"
           priority
         />
       </div>
       
-      <Navbar />
-      <Hero />
+      {/* Content Container - Adjusted for responsive padding */}
+      <div className="relative z-20 h-full px-4 sm:px-6 md:px-12 lg:px-24 xl:px-[180px]">
+        <Navbar />
+        <div className="h-[calc(100vh-80px)]">
+          <Hero />
+        </div>
+      </div>
     </div>
   );
 }
